636de7721986679763cfa1621266735c0108cb36
Sandy Armstrong <sanfordarmstrong@gmail.com>
2013-09-27 20:44:34 -0700

    Initial commit
    Code style is all over the place because I wrote this quickly
    and didn't set up R# yet.
    
    Run `OrbitalCommand.exe mytagname`  from inside a git repo to
    get the short message for all commits going back to `mytagname`,
    with relevant bug numbers next to them. For example:
    
        Fix crash (12345)
        Fix style nit
        Fix two related issues (23456, 34567)
    
    Bug numbers are found based commit messages lines starting with
    "Fixes:" and followed by a FogBugz URL.
    
    The idea is to output some pretty markdown that can be quickly
    edited and used in the body of a Github Release, for example.
    Not quite there yet.

---
+++ OrbitalCommand/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using LibGit2Sharp;
+
+namespace OrbitalCommand
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Too few arguments");
+				return;
+			}
+			var until = args[0];
+
+			using (var repo = new Repository(Environment.CurrentDirectory))
+			{
+				var filter = new CommitFilter
+					{
+						Until = repo.Tags[until],
+						Since = repo.Head,
+						SortBy = CommitSortStrategies.Time
+					};
+
+				var regex = new Regex (@"Fixes:.+\?(?<bugNum>.....)", RegexOptions.Multiline | RegexOptions.Compiled);
+				
+				foreach (var commit in repo.Commits.QueryBy(filter))
+				{
+					var change = new StringBuilder(commit.MessageShort);
+					var matches = regex.Matches(commit.Message);
+					if (matches.Count > 0)
+					{
+						change.Append(" (");
+						var first = true;
+						foreach (Match match in matches)
+						{
+							if (!first)
+								change.Append(", ");
+							change.Append(match.Groups["bugNum"].Value);
+							first = false;
+						}
+						change.Append(")");
+					}
+					Console.WriteLine(change);
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Orbital Command
+
+Automating some tasks in git when my bash skills don't cut it.
+
+First example: extracting bug numbers from commit messages to help
+me write release notes.
